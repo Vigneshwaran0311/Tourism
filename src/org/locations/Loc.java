60a02eeb3858602c7833c52d749d507a97065431
Vigneshwaran0311 <vigneshwaranganesh95@gmail.com>
2020-07-21 21:04:43 +0530

    places added

--- src/org/locations/Loc.java
+++ src/org/locations/Loc.java
@@ -6,5 +6,12 @@ public class Loc {
 		System.out.println("Chennai");
 
 	}
+	public void location2() {
+		System.out.println("Hyderabad");
 
+	}
+	public void location3() {
+		System.out.println("Bengaluru");
+
+	}
 }
